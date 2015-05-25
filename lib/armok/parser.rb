384d27eb581f5f39b649b7966876f255c46eab06
daigidan <daigidan@gmail.com>
2015-05-24 23:33:25 -0700

    Parser refactoring

--- lib/armok/parser.rb
+++ lib/armok/parser.rb
@@ -1,87 +1,19 @@
+$:.unshift File.join(File.dirname(__FILE__))
 require 'citrus'
+require 'parser_extensions'
 
 module Armok
   class Parser
-    attr_reader :match, :input, :name, :type, :definitions
+    include Extensions
+    Citrus.require('dfraw') # load grammar and its namespace
 
-    def initialize(s)
-      Citrus.load('dfraw') # load grammar and its namespace
-      @match = DFRaw.parse(s)
-      @input = s
-      @name = @match.name
-      @type = @match.type
-      @definitions = @match.definitions
+    def parse(s)
+      DFRaw.parse(s)
     end
 
-    def inspect
-      @match
+    def parse_file(filename)
+      DFRaw.parse_file(filename)
     end
 
-    # below are modules which are used to extend the grammar
-    # see dfraw.citrus for details on where they plug in
-
-    module File
-      def to_s
-        s = "#{name}\n\n[OBJECT:#{type}]\n"
-        definitions.each {|d|
-          s << "\n[#{d.subtype}:#{d.id}]\n"
-          d.attributes.each {|a|
-            s << "[#{a.to_s}]\n"
-          }
-        }
-        return s
-      end
-
-      def definitions
-        captures(:definition)
-      end
-
-      def name
-        capture(:file_name_line).capture(:name)
-      end
-
-      def type
-        capture(:declaration).capture(:type)
-      end
-    end
-
-    module Attribute
-      def to_s
-        "#{key}#{values_to_s}"
-      end
-
-      def key
-        capture(:name).value
-      end
-
-      def values
-        captures(:value)
-      end
- 
-      def values_to_s
-        values.map { |v| ':' + v }.join
-      end
-
-      def value
-        captures(:value)[0]
-      end
-    end
-
-    module Object
-      def attributes
-        capture(:attributes).captures(:attribute_line).map {|a|
-          a.capture(:attribute)
-        }
-      end
-
-      def subtype
-        capture(:type)
-      end
-
-      def id
-        capture(:id)
-      end
-    end
-    
   end
 end
