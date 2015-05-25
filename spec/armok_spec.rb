1120a8bbb2b19835e2f293fed2139b0680d6b92e
daigidan <daigidan@gmail.com>
2015-05-24 23:30:28 -0700

    Parser refactoring

--- spec/armok_spec.rb
+++ spec/armok_spec.rb
@@ -43,30 +43,67 @@ item_armor
 [STRUCTURAL_ELASTICITY_CHAIN_ALL]
 EOF
 
-  p = Armok::Parser.new(s)
+  s1 = <<EOF
+b_detail_plan_default
+
+[OBJECT:BODY_DETAIL_PLAN]
+
+[BODY_DETAIL_PLAN:STANDARD_MATERIALS]
+	This command is just a shortcut for the similar lines (USE_MATERIAL_TEMPLATE|<token>|<template>) in a creature definition.
+	[ADD_MATERIAL:SKIN:SKIN_TEMPLATE]
+	[ADD_MATERIAL:FAT:FAT_TEMPLATE]
+	[ADD_MATERIAL:MUSCLE:MUSCLE_TEMPLATE]
+	[ADD_MATERIAL:BONE:BONE_TEMPLATE]
+	[ADD_MATERIAL:CARTILAGE:CARTILAGE_TEMPLATE]
+	[ADD_MATERIAL:HAIR:HAIR_TEMPLATE]
+	[ADD_MATERIAL:TOOTH:TOOTH_TEMPLATE]
+	[ADD_MATERIAL:EYE:EYE_TEMPLATE]
+	[ADD_MATERIAL:NERVE:NERVE_TEMPLATE]
+	[ADD_MATERIAL:BRAIN:BRAIN_TEMPLATE]
+	[ADD_MATERIAL:LUNG:LUNG_TEMPLATE]
+	[ADD_MATERIAL:HEART:HEART_TEMPLATE]
+	[ADD_MATERIAL:LIVER:LIVER_TEMPLATE]
+	[ADD_MATERIAL:GUT:GUT_TEMPLATE]
+	[ADD_MATERIAL:STOMACH:STOMACH_TEMPLATE]
+	[ADD_MATERIAL:GIZZARD:GIZZARD_TEMPLATE]
+	[ADD_MATERIAL:PANCREAS:PANCREAS_TEMPLATE]
+	[ADD_MATERIAL:SPLEEN:SPLEEN_TEMPLATE]
+	[ADD_MATERIAL:KIDNEY:KIDNEY_TEMPLATE]
+	[ADD_MATERIAL:LEATHER:LEATHER_TEMPLATE]
+	[ADD_MATERIAL:TALLOW:TALLOW_TEMPLATE]
+	[ADD_MATERIAL:SOAP:SOAP_TEMPLATE]
+EOF
+
+  parser = Armok::Parser.new
+  o = parser.parse(s)
+#  o1 = parser.parse(s1)
 
   it 'parses embedded filename' do
-    expect(p.name).to eq('item_armor')
+    expect(o.name).to eq('item_armor')
   end
 
   it 'parses object type' do
-    expect(p.type).to eq('ITEM')
+    expect(o.type).to eq('ITEM')
   end
 
   it 'parses definitions' do
-    expect(p.definitions.length).to eq(2)
+    expect(o.definitions.length).to eq(2)
   end
 
   it 'parses attributes' do
-    expect(p.definitions[0].attributes[0].to_s).to eq('NAME:breastplate:breastplates')
+    expect(o.definitions[0].attributes[0].to_s).to eq('NAME:breastplate:breastplates')
   end
 
   it 'parses attributes with multiple values' do
-    expect(p.definitions[0].attributes[0].values.length).to eq(2)
+    expect(o.definitions[0].attributes[0].values.length).to eq(2)
   end
 
   it 'recreates input' do
-    expect(p.to_s).to eq(s)
+    expect(o.to_s).to eq(s)
   end
+
+#  it 'parses definitions with comments' do
+#    expect(p1.to_s).to eq(s1)
+#  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,4 @@
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib', 'armok')
+$:.unshift File.dirname(__FILE__)
 require 'armok'
