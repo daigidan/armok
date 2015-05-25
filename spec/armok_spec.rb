90017d639c4619f27f4f674646ab5123e27a0d7b
daigidan <daigidan@gmail.com>
2015-05-24 19:46:59 -0700

    Add basic parser tests

--- spec/armok_spec.rb
+++ spec/armok_spec.rb
@@ -1,11 +1,59 @@
 require 'spec_helper'
 
 describe Armok do
+  s = <<EOF
+item_armor
+
+[OBJECT:ITEM]
+
+[ITEM_ARMOR:ITEM_ARMOR_BREASTPLATE]
+[NAME:breastplate:breastplates]
+[ARMORLEVEL:3]
+[UBSTEP:0]
+[LBSTEP:0]
+[SHAPED]
+[LAYER:ARMOR]
+[COVERAGE:100]
+[LAYER_SIZE:20]
+[LAYER_PERMIT:50]
+[MATERIAL_SIZE:9]
+[HARD]
+[METAL]
+
+[ITEM_ARMOR:ITEM_ARMOR_MAIL_SHIRT]
+[NAME:mail shirt:mail shirts]
+[ARMORLEVEL:2]
+[UBSTEP:1]
+[LBSTEP:1]
+[LAYER:OVER]
+[COVERAGE:100]
+[LAYER_SIZE:15]
+[LAYER_PERMIT:50]
+[MATERIAL_SIZE:6]
+[HARD]
+[METAL]
+[STRUCTURAL_ELASTICITY_CHAIN_ALL]
+EOF
+
+  p = Armok::Parser.new(s)
+
   it 'has a version number' do
     expect(Armok::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'matches embedded filename' do
+    expect(p.name).to eq('item_armor')
+  end
+
+  it 'matches object type' do
+    expect(p.type).to eq('ITEM')
+  end
+
+  it 'matches definitions' do
+    expect(p.definitions.length).to eq(2)
+  end
+
+  it 'recreates input' do
+    expect(p.to_s).to eq(s)
   end
 end
